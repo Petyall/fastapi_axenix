e276533a12a661814b086888d2e7d4b606b51d18
petyal <alekolen35@gmail.com>
2023-10-22 00:17:12 +0300

    deploy ready

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import settings
 
-DATABASE_URL = "sqlite:///./database.db"
+# DATABASE_URL = "sqlite:///./database.db"
+DATABASE_URL = settings.DATABASE_URL
+
 
 Base = declarative_base()
 
@@ -15,3 +18,19 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+# from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+# from config import settings
+
+
+# # Создание движка для работы
+# engine = create_async_engine(settings.DATABASE_URL)
+# # Создание сессии работы с БД (работает по типу транзакции, т.е. если началось
+# # какое-то действие, то оно должно завершиться. если этого не произошло, то происходит откат)
+# async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+# # Класс для наследоавния моделей и создания миграций
+# class Base(DeclarativeBase):
+#     pass
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Depends, File, UploadFile
 from datetime import datetime
 from database import get_db
 from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
 
 from report.router import router as router_report
 from analyze.router import router as router_analyze
@@ -9,21 +10,29 @@ from analyze.models import ForkliftData
 
 import json
 
-
 app = FastAPI()
+
+origins = ["https://89.108.65.56"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers"],
+)
 
 app.include_router(router_analyze)
 app.include_router(router_report)
 
-@app.post("/import/")
-async def import_json(file: UploadFile = File(...), db: Session = Depends(get_db)):
-    file_content = await file.read()
-    data = json.loads(file_content.decode("utf-8"))
+# @app.post("/import/")
+# async def import_json(file: UploadFile = File(...), db: Session = Depends(get_db)):
+#     file_content = await file.read()
+#     data = json.loads(file_content.decode("utf-8"))
 
-    for item in data:
-        item["time"] = datetime.strptime(item["time"], "%Y-%m-%d %H:%M:%S.%f")
-        forklift_data = ForkliftData(**item)
-        db.add(forklift_data)
-        db.commit()
+#     for item in data:
+#         item["time"] = datetime.strptime(item["time"], "%Y-%m-%d %H:%M:%S.%f")
+#         forklift_data = ForkliftData(**item)
+#         db.add(forklift_data)
+#         db.commit()
 
-    return {"message": "Данные импортированы"}
+#     return {"message": "Данные импортированы"}
